fd87ddd0ef81b5417d42dd9abb5d2ff5da004f35
juncheolkim <kimjoon889@gmail.com>
2023-04-13 21:46:33 +0900

    feat    : 메인페이지 초기화   nextjs는 react문법으로 작성해야 한다. 리액트 버전의 HTML은 JSX라고 부른다. 특징 4개는 다음과 같다. JSX 문법 1. return( ) 안에 HTML 넣을 때 - 평행해서 html 요소를 두 개 이상 작성할 수 없다. return (   <div></div>   <div></div> ) 이처럼 작성 불가능하다. 왜냐하면, return() 안에서는 하나의 HTML태그로 시작해서 하나의 HTML태그로 끝나야 한다는 규칙이 있다.
    JSX 문법 2. class 말고 className
    왜냐하면, javascript 안에서 작성하기 때문에 js 기존에 있는 class 문법과 구분하기 위해서 className으로 사용
    
    JSX 문법 3. html 안에 변수 넣을 땐 { }
    JSX의 데이터바인딩 문법
    HTML 태그 내용에 데이터를 바인딩 할 수도 있지만,
    HTML 속성 안에 변수를 넣을 수도 있다.
    
    JSX 문법 4. style 넣고 싶으면 object 자료형으로
    주의점 : javascript object 자료형이기 때문에 key값에 '-' 기호를 사용할 수 없다.

--- app/page.js
+++ app/page.js
@@ -2,8 +2,19 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
+// 계층적으로 layout.js 안에 page.js가 존재한다.
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <div></div>;
+    let name = "park";
+    let link = "http://google.com";
+    return (
+        <div>
+            <h4 style={{ color: "red", textAlign: "center", fontSize: "30px" }}>
+                로고
+            </h4>
+            <p className="title-sub">by dev {name}</p>
+            <a href={link}>링크</a>
+        </div>
+    );
 }
