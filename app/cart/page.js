34855957ff9300c390ebe0bc51ed51f6d99eeea0
juncheolkim <kimjoon889@gmail.com>
2023-05-01 18:54:57 +0900

    feat    : useState 데이터를 잠깐 저장하기 위해 변수가 아닌 'state'를 사용할 수 있다. state를 사용하는 이유 state 변경되면 state 사용하는 html 부분도 자동 재렌더링된다. 일반 변수를 사용할 경우 html이 재렌더링 되지 않는다.

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,4 +1,3 @@
-import { age, name } from "./data";
 import Hello from "./hello";
 
 export default function Cart() {
@@ -17,10 +16,6 @@ export default function Cart() {
     );
 }
 
-// props 문법으로 부모 -> 자식 데이터 전송하려면
-// 1. 자식컴포넌트 사용하는 곳에 가서 <자식컴포넌트 item={전해줄데이터} />
-// 2. 자식컴포넌트 정의부분으로 가서 props라는 파라미터 등록 후 props.item 사용
-
 function CartItem(props) {
     return (
         <div className="cart-item">
@@ -30,9 +25,6 @@ function CartItem(props) {
         </div>
     );
 }
-// 자식 -> 부모 데이터 전송 불가능
-// 형제 태그에 데이터 전송 불가능
-// 항상 부모에서 자식에게로만 데이터 전송 가능!
 
 function Banner(props) {
     return <h5 className="title-sub">{props.cardName} 결제 행사중</h5>;
--- app/lists/page.js
+++ app/lists/page.js
@@ -1,15 +1,42 @@
+// onClick을 사용하기 위해서 client component로 만들어야 한다.
+"use client";
+
+import { useState } from "react";
+
 export default function Lists() {
     let 상품 = ["Tomato", "Pasta", "Coconut"];
-
+    // 데이터를 잠깐 저장하기 위해 변수가 아닌 'state'를 사용할 수 있다.
+    // state를 사용하는 이유
+    // state 변경되면 state 사용하는 html 부분도 자동 재렌더링된다.
+    // 일반 변수를 사용할 경우 html이 재렌더링 되지 않는다.
+    // let [수량, 수량변경] = useState(0);
     return (
         <div>
             <h4 className="title">상품목록</h4>
 
             {상품.map((src, idx) => {
+                let [수량, 수량변경] = useState(0);
                 return (
                     <div className="food" key={idx}>
                         <img src={`/food${idx}.png`} className="food-img" />
                         <h4>{상품[idx]} $40</h4>
+                        <button
+                            onClick={() => {
+                                if (수량 > 0) {
+                                    수량변경(수량 - 1);
+                                }
+                            }}
+                        >
+                            -
+                        </button>
+                        <span> {수량} </span>
+                        <button
+                            onClick={() => {
+                                수량변경(수량 + 1);
+                            }}
+                        >
+                            +
+                        </button>
                     </div>
                 );
             })}
